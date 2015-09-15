6d793a49773b7866e04cc31379dbaccda23d0446
Jamargle <jmlb0003@red.ujaen.es>
2015-09-15 18:25:00 +0200

    Working /all_pois URL

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
+var path = require('path');
+console.log("Ha llegao al models/index.js y el global.... es:"+!global.hasOwnProperty('db'));
 if (!global.hasOwnProperty('db')) {
   var Sequelize = require('sequelize')
-    , sequelize = null
-
+    , sequelize = null;
+console.log("el HEROKU.... es:"+process.env.HEROKU_POSTGRESQL_BRONZE_URL);
   if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
     // the application is executed on Heroku ... use the postgres database
     var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
@@ -15,13 +17,18 @@ if (!global.hasOwnProperty('db')) {
     })
   } else {
     // the application is executed on the local machine ... use mysql
-    sequelize = new Sequelize('example-app-db', 'root', null)
+    sequelize = new Sequelize("basededatosespacial", "root", "mysql1234");
   }
+console.log("sequelize vale:"+sequelize+"\ny __dirname:"+__dirname+"\ny pathjoin:"+poi_path);
+  // Importar definicion de la tabla Poi
+  var poi_path = path.join(__dirname,'poi.js');
+  console.log("sequelize vale:"+sequelize+"\ny __dirname:"+__dirname+"\ny pathjoin:"+poi_path);
+//  var Poi = sequelize.import(poi_path);
 
   global.db = {
     Sequelize: Sequelize,
     sequelize: sequelize,
-    User:      sequelize.import(__dirname + '/user') 
+    Poi:      sequelize.import(poi_path) 
     // add your other models here
   }
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var poisController = require('../controller/poi_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+//==========================================
+// Rutas disponibles
+//==========================================
+
+//Queries
+//GET all_pois	-> it returns all pois
+//GET pois_by_location/:position?	-> it returns some pois near by a location given by its GPS coordinates
+
+// Definición de rutas de /pois
+router.get('/all_pois', poisController.show_all);
+router.get('/pois_by_location/:position?', poisController.pois_by_location);
+
+//POST create_poi	-> Create a new poi
+//GET pois/:id?/edit	-> Edits a poi
+//PUT pois/:id?	-> Update a poi
+//DELETE pois/:id?	-> Delete a poi
+router.post('/create_poi', poisController.create);
+router.get('/pois/:id?/edit', poisController.edit);
+router.put('/pois/:id?', poisController.update);
+router.delete('/pois/:id?', poisController.destroy);
+
+
 
 module.exports = router;
